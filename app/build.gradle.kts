940d9b685648dd236051e1550127d6d9d22ca837
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-05-10 14:33:07 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,11 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.7"
 
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
