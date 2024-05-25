793e8d640e26fee562d643d74e0c68384976f397
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2024-05-25 15:37:42 +0300

    modified MainActivity.kt

--- app/src/main/java/com/cenkeraydin/restaurantfinder/MainActivity.kt
+++ app/src/main/java/com/cenkeraydin/restaurantfinder/MainActivity.kt
@@ -3,28 +3,54 @@ package com.cenkeraydin.restaurantfinder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.cenkeraydin.restaurantfinder.ui.theme.RestaurantFinderTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.cenkeraydin.restaurantfinder.screens.EntryScreen
+import com.cenkeraydin.restaurantfinder.screens.FavoriteScreen
+import com.cenkeraydin.restaurantfinder.screens.HomeScreen
+import com.cenkeraydin.restaurantfinder.screens.LoginScreen
+import com.cenkeraydin.restaurantfinder.screens.SignUpScreen
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+    private val auth: FirebaseAuth by lazy { Firebase.auth }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RestaurantFinderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
-            }
+            MyApp()
         }
     }
 }
+
+@Composable
+fun MyApp() {
+    val viewModel: RestaurantViewModel = viewModel()
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "EntryScreen") {
+        composable("EntryScreen") {
+            EntryScreen(navController = navController)
+        }
+        composable("LoginScreen") {
+            LoginScreen(navController)
+        }
+        composable("SignUpScreen") {
+            SignUpScreen(navController)
+        }
+        composable("HomeScreen"){
+            HomeScreen(navController = navController,viewModel)
+        }
+        composable("FavoriteScreen"){
+            FavoriteScreen(navController,viewModel)
+        }
+
+    }
+}
+
+
+
